e85da4b9a4b372bb56bf27b3dbb209e6f1a10ad9
Revan <revankumars332@gmail.com>
2025-01-20 18:24:02 +0530

    darte Range Selection Api parameter Change

--- src/api/dashboardApi/index.js
+++ src/api/dashboardApi/index.js
@@ -1,8 +1,8 @@
 import axiosInstance from '../config/axiosConfig';
 
-export const  fetchDashboardCountDetail = async (id) => {
+export const  fetchDashboardCountDetail = async (id,startDate,endDate) => {
     try {
-        const response = await axiosInstance.get(`/Customer/GetDashboardCountDetail?CustomerId=${id}`);
+        const response = await axiosInstance.get(`/Customer/GetDashboardCountDetail?CustomerId=${id}&StartDate=${startDate}&EndDate=${endDate}`);
         return response.data; 
     } catch (error) {
         console.error('Error fetching customer files:', error);
